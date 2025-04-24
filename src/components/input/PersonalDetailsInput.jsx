857d8f2cb9351fe23d61be3bd24f10b80f26b555
Chenhao Wang <implexrr@gmail.com>
2025-04-23 20:38:20 -0400

    feat(input): add functionality to show more/show less button

--- src/components/input/PersonalDetailsInput.jsx
+++ src/components/input/PersonalDetailsInput.jsx
@@ -1,70 +1,86 @@
+import { useState } from "react";
+
 const PersonalDetailsInput = ({ personalDetails, setPersonalDetails }) => {
-  const handleChange = e => {
+
+  const [isExpanded, setIsExpanded] = useState(true)
+
+  const handleToggle = () => {
+    setIsExpanded(expanded => !expanded)
+  }
+
+  function handleChange(e) {
     const { name, value } = e.target
     setPersonalDetails({
       ...personalDetails,
       [name]: value
     })
   }
+
   return (
     <form id="personalDetails">
       <div className="formLabel">
         <div className="formLabel icon"></div>
         <div className="formLabel title">Personal Details</div>
-        <button type="button" className="showMore icon"></button>
+        <button type="button" className={isExpanded ? "showLess icon" : "showMore icon"} onClick={handleToggle}></button>
       </div>
+      {
+        isExpanded && (
+          <>
+          <div id="fullNameInput" className="personalInput">
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              type="text"
+              id="fullName"
+              name="fullName"
+              placeholder="Bob McJobhunter"
+              defaultValue={personalDetails.fullName}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-      <div id="fullNameInput" className="personalInput">
-        <label htmlFor="fullName">Full Name</label>
-        <input
-          type="text"
-          id="fullName"
-          name="fullName"
-          placeholder="Bob McJobhunter"
-          defaultValue={personalDetails.fullName}
-          onChange={handleChange}
-          required
-        />
-      </div>
+          <div id="addressInput" className="personalInput">
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              placeholder="123 Maplevood Rd, ON, Canada"
+              defaultValue={personalDetails.address}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-      <div id="addressInput" className="personalInput">
-        <label htmlFor="address">Address</label>
-        <input
-          type="text"
-          id="address"
-          name="address"
-          placeholder="123 Maplevood Rd, ON, Canada"
-          defaultValue={personalDetails.address}
-          onChange={handleChange}
-          required
-        />
-      </div>
+          <div id="phoneInput" className="personalInput">
+            <label htmlFor="phone">Phone Number</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              placeholder="647-967-1111"
+              defaultValue={personalDetails.phone}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-      <div id="phoneInput" className="personalInput">
-        <label htmlFor="phone">Phone Number</label>
-        <input
-          type="tel"
-          id="phone"
-          name="phone"
-          placeholder="647-967-1111"
-          defaultValue={personalDetails.phone}
-          onChange={handleChange}
-          required
-        />
-      </div>
+          <div id="emailInput" className="personalInput">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="pleasehireme42@gmail.com"
+              name="email"
+              defaultValue={personalDetails.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          </>
+        )
+      }
 
-      <div id="emailInput" className="personalInput">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          placeholder="pleasehireme42@gmail.com"
-          name="email"
-          defaultValue={personalDetails.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
     </form>
   );
 };
